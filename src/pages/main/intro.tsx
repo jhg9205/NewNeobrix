cfbebfbb0ae55f865c2737e8389cbc6b434615d0
jsh <jhg9205@nate.com>
2024-11-06 16:33:56 +0900

    네오브릭스 리뉴얼 (2024-11-06)

--- src/pages/main/intro.tsx
+++ src/pages/main/intro.tsx
@@ -1,15 +1,17 @@
 import * as React from 'react'
 import { useNavigate } from 'react-router-dom'
-import {motion, useScroll, useTransform, useViewportScroll, Variants} from "framer-motion"
+import {color, motion, useScroll, useTransform, useViewportScroll, Variants} from "framer-motion"
 import {useEffect, useState} from "react";
 import Transition from '@components/ui/transition/transition'
+import {Button} from "@mui/material";
+import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
 
 export const Intro = () => {
         const [scrollYProgress, setScrollYProgress] = useState(7);
 
         const handleScroll = () => {
             const scrollTop = window.scrollY || document.documentElement.scrollTop;
-            console.log(scrollTop);
+
             if(scrollTop > 500){
                 setScrollYProgress(500);
             }else{
@@ -45,7 +47,7 @@ export const Intro = () => {
                 id='intro-container'
                 style={{
                     width:`${scrollYProgress*10+20}%`,
-                    height:'2000px',
+                    height:'1000px',
                     borderRadius: '30px',
                     background: 'linear-gradient(to bottom, #74f8c2, #00e2d3, #00c9de, #00addf, #0090d2, #3f8ed0, #5a8bcd, #6e89c9, #99a4d6, #bec1e3, #e0dff1, #ffffff)'
                 }}
@@ -88,7 +90,18 @@ export const Intro = () => {
                                     transform:'translateY(-10px)'
                                 }}
                             >
-                                <p>CONTENT1</p>
+                                <Button
+                                    style={{cursor:'pointer',height:'100%', width:'100%'}}>
+                                    <div
+                                        className='intro-card-button'
+                                        style={{verticalAlign:'bottom',display:'table-cell', padding:'0 40px',width:'100%'}}>
+                                    <p>SI(시스템통합)</p>
+                                    <span>지속적 계발과 안정적인 기술인력을 통해 컨설팅, 시스템 구축, 운영 전반의 최고의 서비스를 제공합니다.</span>
+                                    <div className='intro-card-arrow'>
+                                        <ArrowForwardIcon fontSize='large' sx={{color:'white', fontSize:'50px'}}/>
+                                    </div>
+                                    </div>
+                                </Button>
                             </motion.div>
                         </>
                     </Transition>
@@ -106,7 +119,18 @@ export const Intro = () => {
                                     transform:'translateY(+90px)'
                                 }}
                             >
-                                <p>CONTENT1</p>
+                            <Button
+                                style={{cursor:'pointer',height:'100%', width:'100%'}}>
+                                <div
+                                    className='intro-card-button'
+                                    style={{verticalAlign:'bottom',display:'table-cell', padding:'0 40px',width:'100%'}}>
+                                    <p>AI분석 플랫폼(HPE)</p>
+                                    <span>HPE Ezmeral Big-Data 플랫폼 (HPE EZMERAL Data Fabric)</span>
+                                    <div className='intro-card-arrow'>
+                                        <ArrowForwardIcon fontSize='large' sx={{color:'white', fontSize:'50px'}}/>
+                                    </div>
+                                </div>
+                            </Button>
                             </motion.div>
                         </>
                     </Transition>
@@ -124,7 +148,18 @@ export const Intro = () => {
                                     transform:'translateY(+90px)'
                                 }}
                             >
-                                <p>CONTENT1</p>
+                                <Button
+                                    style={{cursor:'pointer',height:'100%', width:'100%'}}>
+                                    <div
+                                        className='intro-card-button'
+                                        style={{verticalAlign:'bottom',display:'table-cell', padding:'0 40px',width:'100%'}}>
+                                    <p>빅데이터 플랫폼(HPE)</p>
+                                    <span>HPE Ezmeral AI 플랫폼 (HPE EZMERAL ML OPS Platform)</span>
+                                    <div className='intro-card-arrow'>
+                                        <ArrowForwardIcon fontSize='large' sx={{color:'white', fontSize:'50px'}}/>
+                                    </div>
+                                    </div>
+                                </Button>
                             </motion.div>
                         </>
                     </Transition>
@@ -142,7 +177,18 @@ export const Intro = () => {
                                     transform:'translateY(+190px)'
                                 }}
                             >
-                                <p>CONTENT1</p>
+                                <Button
+                                    style={{cursor:'pointer',height:'100%', width:'100%'}}>
+                                    <div
+                                        className='intro-card-button'
+                                        style={{verticalAlign:'bottom',display:'table-cell', padding:'0 40px',width:'100%'}}>
+                                    <p>스마트 안전보건관리 솔루션</p>
+                                    <span>중대재해 발생 시 법적대응에 활용할 수 있는 솔루션</span>
+                                    <div className='intro-card-arrow'>
+                                        <ArrowForwardIcon fontSize='large' sx={{color:'white', fontSize:'50px'}}/>
+                                    </div>
+                                    </div>
+                                </Button>
                             </motion.div>
                         </>
                     </Transition>
