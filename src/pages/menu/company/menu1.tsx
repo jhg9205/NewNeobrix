670280df4d41d8ceebec567b8a1ff4619e0759c7
jsh <jhg9205@nate.com>
2024-11-11 16:49:34 +0900

    네오브릭스 리뉴얼 (2024-11-11)

--- src/pages/menu/company/menu1.tsx
+++ src/pages/menu/company/menu1.tsx
@@ -2,17 +2,19 @@ import Layout from '@components/layouts/layout'
 import FadeImg from '@components/ui/effect/fadeImg'
 import Transition from '@components/ui/transition/transition'
 
-import main1 from '@images/menu/company-main1.png'
-import main2 from '@images/menu/company-main2.png'
+import SystemUpdateAltIcon from '@mui/icons-material/SystemUpdateAlt';
 import introImg from '@images/common/img-intro2.png'
 import React from 'react'
-import { Fade, Grid } from '@mui/material'
+import {Button, Fade, Grid} from '@mui/material'
 import MenuListbar from './menuListbar'
 import { PATH } from '@common/domain'
 import Organize from '@images/common/organize.jpg'
 import {Autoplay, EffectFade, Navigation, Pagination} from "swiper";
 import {Swiper, SwiperSlide} from "swiper/react";
 import { motion } from 'framer-motion'
+import {alert} from "@utils/alert";
+import {ALERT} from "@common/const";
+import {$FileDownLoad} from "@utils/request";
 
 const Menu1 = () => {
 	const [ceo, setCeo] = React.useState(false)
@@ -26,7 +28,20 @@ const Menu1 = () => {
 	const callEndFunc = () => {
 		setCeo(true)
 	}
+	const handleClick = (event: React.MouseEvent) => {
 
+		let file = '/download/NEOBRIX.pdf'
+		let name = 'NEOBRIX_회사소개서'
+		let type = 'pdf'
+		alert.confirm({
+			type: ALERT.CONFIRM,
+			text: `'${name}' 를\n 저장 하시겠습니까?\n\n`,
+			confirmText: '확인',
+			confirmCall: () => {
+				$FileDownLoad(file, name, type)
+			}
+		})
+	}
 	const menulist = MenuListbar(PATH.COMPANY00)
 
 	return  (
@@ -92,83 +107,42 @@ const Menu1 = () => {
 						<Transition threshold={-1} direction={'up'}>
 							<Grid container spacing={0}>
 								<Grid item lg={6} md={12}>
-									<div className="ceo_img">
-										<FadeImg pc={introImg} delay={1500} isContent={true} />
-									</div>
-								</Grid>
-								<Grid item lg={6} md={12}>
 									<Fade in={true} timeout={1500}>
 										<div className="ceo_area">
 											<div className="ceo_tit">
-												안녕하십니까!
+												Neo BRIX의 기술과 서비스는
 												<br />
-												<span className="pointer">NEOBRIX</span>를 방문해주셔서 감사합니다.
+												고객의 디지털 트랜스포메이션 실현을 위한
+												<br/>
+												모든 서비스를 제공합니다.
 											</div>
 											<div className="ceo_txt">
 												<p>
-													네오브릭스는 SI사업을 통한 최적화된 시스템 및 Big-Data/AI플랫폼을 제공하는 ICT 전문기업입니다.
+													디지털적이 모든 것으로 발생하는 다양한 변화에 맞춰 디지털 기반으로 기업의
 													<br />
+													전략, 조직, 프로세스, 커뮤니케이션, 문화, 시스템, 가치사슬, 비지니스 모델을
 													<br />
-													4차 산업 분야의 다양한 융복합 기술을 기반으로 GIS, 토지정보, 공공 서비스 및 상하수도, 물관리 분야의 시스템 통합 전문 기업이며,
+													근본적으로 변화시키기 위한 기술과 전문성을 제공합니다.
 													<br />
-													<br />
-													대용량 데이터관리 역량을 바탕으로 빅데이터 시스템 마스터 플랜 수립, 빅데이터 인프라 아키텍처 설계, 빅데이터 거버넌스 및 분석 컨설팅을 주 사업 영역으로 제공하고 있습니다.
-													<br />
-													<br />
-													또한 최신 ICT기술을 적용한 신규 솔루션 및 서비스 제공을 위한 지속적인 활동을 하고, 차별화된 고객 서비스 제공을 위해 항상 노력하고 고민하는 기업으로 발전해 가고자 합니다.
-													<br />
-													<br />
-													감사합니다.
+													새로운 경험과 혁신을 Neo BRIX와 함께 합니다.
 												</p>
-												<span className="ceo_sign">네오브릭스 임직원 일동</span>
+											</div>
+
+											<div onClick={handleClick} className='download-div'>
+												<SystemUpdateAltIcon/>
+												<span className='download'>회사소개서 다운로드</span>
 											</div>
 										</div>
 									</Fade>
 								</Grid>
+								<Grid item lg={6} md={12}>
+									<div className="ceo_img">
+										<FadeImg pc={introImg} delay={1500} isContent={true} />
+									</div>
+								</Grid>
 							</Grid>
 						</Transition>
 					</div>
-						<Transition threshold={4} direction={'up'}>
-								<Grid container>
-									<Grid item lg={6} md={12} style={{backgroundColor:'#2A323F', width:'100%', padding:'30px'}} className='introOrganize'>
-										<p className="company_desc_p ">
-											<span className="title">회사명</span>
-											<span className="desc">네오브릭스 주식회사</span>
-										</p>
-										<p className="company_desc_p ">
-											<span className="title">설립일</span>
-											<span className="desc">2024년 7월 8일</span>
-										</p>
-										<p className="company_desc_p ">
-											<span className="title">대표이사</span>
-											<span className="desc">현 종 일</span>
-										</p>
-										<p className="company_desc_p ">
-											<span className="title">사업영역</span>
-											<span className="desc">소프트웨어 개발, 공급 및 유지보수<br/>시스템 통합 자문 및 구축 관련 서비스 용역</span>
-										</p>
-										<p className="company_desc_p ">
-											<span className="title">임직원수</span>
-											<span className="desc">18명 (2024년 8월 기준)</span>
-										</p>
-										<p className="company_desc_p ">
-											<span className="title">연구개발<br/>/기술인력</span>
-											<span className="desc">14명 (2024년 1월 기준)</span>
-										</p>
-										<p className="company_desc_p ">
-											<span className="title">위치</span>
-											<span className="desc">(본사)서울특별시 송파구 백제고분로7길 16-9, 한신빌딩 4층 401호</span>
-										</p>
-										<p className="company_desc_p ">
-											<span className="title">대표전화</span>
-											<span className="desc">02-423-7860</span>
-										</p>
-									</Grid>
-									<Grid item lg={6} md={12} style={{backgroundColor:'#2A323F', width:'100%'}}>
-										<img className='organize' src={Organize} alt='dd'/>
-									</Grid>
-								</Grid>
-						</Transition>
 				</div>
 			</div>
 		</Layout>
