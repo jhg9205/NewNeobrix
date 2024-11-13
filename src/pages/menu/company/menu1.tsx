32d4e2d01cda6e5b016ad68c6c450dd5ecbaeb57
jsh <jhg9205@nate.com>
2024-11-13 09:12:17 +0900

    네오브릭스 리뉴얼 (2024-11-13)

--- src/pages/menu/company/menu1.tsx
+++ src/pages/menu/company/menu1.tsx
@@ -21,7 +21,7 @@ const Menu1 = () => {
 
 	const style = {
 		width: '100%',
-		minHeight: '800px',
+		minHeight: '600px',
 		minWidth: '100%'
 	}
 
@@ -142,6 +142,47 @@ const Menu1 = () => {
 								</Grid>
 							</Grid>
 						</Transition>
+					</div>
+				</div>
+				<div className='greeting'>
+					<div className='greeting-main'>
+						Greetings
+					</div>
+					<div className='greeting-sub'>
+						<p>자율-창조적인 마인드와 지속적인 기술개발을 통해</p>
+						<p>손에 잡히는 IT서비스 제공으로 고객의 성장을 보상하겠습니다.</p>
+					</div>
+					<div className='greeting-content'>
+						<p>
+							NeoBRIX는 2024년 4차 사업 분야에 필요한 다양한 융복합 기술을 제공할 수 있는 전문가들로 구성하여 설립하였으며, 고객만족과 신뢰를
+						</p>
+						<p>
+							최우선 가치로 서비스를 제공합니다.
+						</p>
+						<p>
+							특히, 변화되는 디지털 환경에 맞게 단순한 기술도입 아닌 비즈니스 모델과 조직문화를 재구성할 수 있는 디지털 트랜스포메이션(DX)
+						</p>
+						<p>
+							서비스를 공공 정보화사업에 제공하고자 합니다.
+						</p>
+						<p>
+							이를 위해 고객이 언제 어디서나 일관된 채널을 통합(SI)하여 고객 데이터 분석(Big Data),
+						</p>
+						<p>
+							실시간 상호 작용(Data Mining AI)지원하여 고객 만족도를 높이 높이겠습니다.  이러한 NeoBRIX의 서비스 형태는
+						</p>
+						<p>
+							시스템 통합(SI), 아웃소싱(ITO), IT컨설팅, 솔루션 도입 등 다양한 사업 형태로 제공 될 것입니다.
+						</p>
+						<p>
+							앞으로도 NeoBRIX는 지속적인 자율-창조적인 마인드의 인재 양성과 지속적인 기술개발 통해 고객 자원의
+						</p>
+						<p>
+							효율성 및 생산성을 높여 고객의 성장을 보장하는 혁신기업이 되겠습니다.
+						</p>
+						<p>
+							감사합니다.
+						</p>
 					</div>
 				</div>
 			</div>
