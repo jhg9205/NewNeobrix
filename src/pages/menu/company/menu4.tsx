17ad477375c3756c1ff609bd88d5427b70c990fd
jsh <jhg9205@nate.com>
2024-11-13 16:50:00 +0900

    네오브릭스 리뉴얼 (2024-11-13)

--- src/pages/menu/company/menu4.tsx
+++ src/pages/menu/company/menu4.tsx
@@ -9,6 +9,10 @@ import React from 'react'
 import { PATH } from '@common/domain'
 import MenuListbar from './menuListbar'
 import {Divider} from "@mui/material";
+import {alert} from "@utils/alert";
+import {ALERT} from "@common/const";
+import {$FileDownLoad} from "@utils/request";
+import SystemUpdateAltIcon from "@mui/icons-material/SystemUpdateAlt";
 
 const Menu4 = () => {
 	const [location, setLocation] = React.useState(false)
@@ -22,7 +26,20 @@ const Menu4 = () => {
 	const callEndFunc = () => {
 		setLocation(true)
 	}
+	const handleClick = (event: React.MouseEvent) => {
 
+		let file = '/download/NEOBRIX.pdf'
+		let name = 'NEOBRIX_회사소개서'
+		let type = 'pdf'
+		alert.confirm({
+			type: ALERT.CONFIRM,
+			text: `'${name}' 를\n 저장 하시겠습니까?\n\n`,
+			confirmText: '확인',
+			confirmCall: () => {
+				$FileDownLoad(file, name, type)
+			}
+		})
+	}
 	const subTileTrans = (
 		<Transition threshold={-1} direction={'up'} isEndListener={true} callFunc={callEndFunc} time={1500}>
 			<div className="menu_title_p_fixed_warp">
@@ -38,15 +55,23 @@ const Menu4 = () => {
 
 	return (
 		<Layout>
-			<div>
-				<div>
+			<div style={{padding:'150px 300px 0 300px'}} className='menu-header'>
+				<div style={{fontSize:'60px', fontWeight:'600'}}>
 					오시는길
 				</div>
-				<div>
-					네오브릭스 오시는 길
+				<div style={{fontSize:'20px',fontWeight:'400',margin:'20px 0 50px 0'}}>
+					<span>네오브릭스 오시는 길</span>
+					<div onClick={handleClick} className='download-div'>
+						<SystemUpdateAltIcon/>
+						<span className='download'>회사소개서 다운로드</span>
+					</div>
 				</div>
-				<Divider/>
 			</div>
+			<Divider/>
+			<div style={{height:'50px', paddingLeft:'300px',verticalAlign:'middle'}}>
+				<p style={{alignContent:'center', fontSize:'20px'}}>기업소개 &gt; 오시는길</p>
+			</div>
+			<Divider/>
 			<div id="locationLayout">
 				<div className="menu_title_contain" style={style}>
 					{subTileTrans}
